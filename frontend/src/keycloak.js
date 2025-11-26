1f697f00d517940ab9138af47ddbd674bd534270
Lucas Bogado <lubogado@frba.utn.edu.ar>
2025-11-25 21:00:33 -0300

    Implement mock Keycloak for disabled authentication
    Add mock Keycloak implementation when auth is disabled

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -1,7 +1,17 @@
 
 import Keycloak from 'keycloak-js';
 
+if (process.env.REACT_APP_DISABLE_AUTH === "true") {
+  export const keycloak = {
+    init: () => Promise.resolve(true),
+    authenticated: true,
+    token: "fake-token",
+    login: () => {},
+    logout: () => {},
+  };
 
+  export default keycloak;
+}
 const keycloakConfig = {
   url: 'http://localhost:8080', 
   realm: 'tp-tienda-sol',        
